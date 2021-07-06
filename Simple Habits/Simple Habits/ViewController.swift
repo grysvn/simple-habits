2eba700befed9a7e402abc746760d5398e428153
Matthew Gray <matthew.gray@willowtreeapps.com>
2021-07-06 06:56:42 -0400

    Initial xcode commit.

---
+++ Simple Habits/Simple Habits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Habits
+//
+//  Created by Matthew Gray on 7/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
